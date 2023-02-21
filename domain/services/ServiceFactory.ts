aca96f99d3888cc3884ee37e067486d5feb78462
Jerneja <bozic.jerneja@gmail.com>
2023-02-21 18:05:06 +0000

    Allow 0 or multiple arguments.

--- domain/services/ServiceFactory.ts
+++ domain/services/ServiceFactory.ts
@@ -1,7 +1,7 @@
 import ServiceGetPeople from "./ServiceGetPeople";
 import PeopleGraphQLRepository from "../../app/gateways/repositories/PeopleGraphQLRepository";
 
-const getPeople = () => {
+const getPeople = (args?) => {
   let service = new ServiceGetPeople();
   service.peopleRepository = new PeopleGraphQLRepository();
   return service;
@@ -12,7 +12,7 @@ let factories = {
 };
 
 class ServiceFactory {
-  static instanceFor = (serviceClass, args?) => {
+  static instanceFor = (serviceClass, ...args: any[]) => {
     try {
       let serviceInstance = factories[serviceClass.name](args);
       return serviceInstance;
