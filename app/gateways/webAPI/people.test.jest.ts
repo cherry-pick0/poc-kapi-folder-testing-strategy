2d727c4379cd37fa1a29079a6f0cb6ef1130d6cb
Jerneja <bozic.jerneja@gmail.com>
2023-02-23 15:04:09 +0000

    Update simple jest and mocha test for /people endpoint.

--- app/gateways/webAPI/people.test.jest.ts
+++ app/gateways/webAPI/people.test.jest.ts
@@ -6,5 +6,10 @@ describe("People test flow", () => {
   it("tests /people endpoint", async () => {
     const response = await request(app).get("/people")
     expect(response.statusCode).toBe(200)
+    expect(Array.isArray(response.body)).toBeTruthy()
+    expect(response.body.length).toBeGreaterThanOrEqual(1)
+    expect(typeof response.body[0]).toBe('object')
+    expect(response.body[0]).toHaveProperty('id')
+    expect(response.body[0]).toHaveProperty('name')
   })
 })
--- app/gateways/webAPI/people.test.mocha.ts
+++ app/gateways/webAPI/people.test.mocha.ts
@@ -1,4 +1,4 @@
-import chai, {expect} from "chai"
+import chai, { expect } from "chai"
 import chaiHttp from "chai-http"
 
 const app = require("../../../index")
@@ -8,5 +8,10 @@ describe("GET /people", () => {
   it("List all people", async () => {
     const response = await chai.request(app).get("/people")
     expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.a('array')
+    expect(response.body).length.to.be.greaterThanOrEqual(1)
+    expect(response.body[0]).to.be.a('object')
+    expect(response.body[0]).to.have.property('id')
+    expect(response.body[0]).to.have.property('name')
   })
 })
