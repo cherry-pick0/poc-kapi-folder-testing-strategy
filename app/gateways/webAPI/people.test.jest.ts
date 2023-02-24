71397e8726d66a0f4e4bd317ce18b70630834784
Jerneja <bozic.jerneja@gmail.com>
2023-02-24 12:32:45 +0000

    Add API test for creating a person.

--- app/gateways/webAPI/people.test.jest.ts
+++ app/gateways/webAPI/people.test.jest.ts
@@ -8,8 +8,19 @@ describe("People test flow", () => {
     expect(response.statusCode).toBe(200)
     expect(Array.isArray(response.body)).toBeTruthy()
     expect(response.body.length).toBeGreaterThanOrEqual(1)
-    expect(typeof response.body[0]).toBe('object')
-    expect(response.body[0]).toHaveProperty('id')
-    expect(response.body[0]).toHaveProperty('name')
+    expect(typeof response.body[0]).toBe("object")
+    expect(response.body[0]).toHaveProperty("id")
+    expect(response.body[0]).toHaveProperty("name")
+  })
+
+  it("tests POST /people endpoint", async () => {
+    const data = { name: "Rick" }
+    const response = await request(app).post("/people", data)
+    expect(response.statusCode).toBe(201)
+    expect(typeof response.body).toBe("object")
+    expect(response.body).toHaveProperty("id")
+    expect(response.body).toHaveProperty("name")
+    expect(response.body).toHaveProperty("email_address")
+    expect(response.body["name"]).toBe("Rick")
   })
 })
