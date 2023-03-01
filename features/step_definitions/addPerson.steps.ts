16bd39fe6aaa4350d8db78e8a3f0c9b45489ea01
Jerneja <bozic.jerneja@gmail.com>
2023-03-01 10:20:27 +0000

    Reproduce an existing test.

--- features/step_definitions/addPerson.steps.ts
+++ features/step_definitions/addPerson.steps.ts
@@ -1,8 +1,26 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then } = require("@cucumber/cucumber")
+const {
+  ServiceAddPerson,
+  AddPersonIPeopleRepo,
+} = require("../../domain/services/ServiceAddPerson")
+const { Person } = require("../../domain/entities/Person")
+const { AddPersonResponse } = require("../../app/gateways/responses/index")
 
-When("we create a person", function () {
+When("we create a person", async function () {
   // Write code here that turns the phrase above into concrete actions
-  return "pending"
+  const testPeopleRepo: AddPersonIPeopleRepo = {
+    addPerson: async (person: Person): Promise<AddPersonResponse> => {
+      return {
+        id: person.id,
+        name: person.name,
+        email_address: person.emailAddress?.EmailAddressSchema.address,
+      }
+    },
+  }
+  const service = ServiceAddPerson(testPeopleRepo)
+  let person: Person = { name: "Rick" }
+  let data = await service.execute(person)
+  return data
 })
 
 Then("we should receive", function (dataTable) {
